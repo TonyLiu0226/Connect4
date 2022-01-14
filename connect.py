30f4e6c53eda012f3b96ef66db84b87be1d0dd01
Tony Liu <tl0226yn@gmail.com>
2022-01-13 23:07:33 -0800

    Implements the game based on keyboard input

--- connect.py
+++ connect.py
@@ -9,24 +9,44 @@ class board():
         try:
             self.length = length
             self.width = width
-
-            if (self.length < 5 or self.width < 5):
-                raise Exception("Size is not big enough")
        
             #initializes a 2D integer array that is equivalent to the board
-            #integer keys: 0 is unoccupied, 1 is occupied by player 1, and 2 is occupied by player 2
+            #integer keys: 0 is unoccupied, 1 is occupied by player 1, and 2 is occupied by player 2, etc... Max 9 players
             #initially sets the board to all 0's
             self.board = [[0 for x in range(length)]for y in range(width)]
+            self.gameOver = False
+
         except Exception as e:
             print(e)
 
     #adds a counter to the correct column number    
     def addCounter(self, playerNum, colNum):
-        #checks if column is full. If full, do not allow user to place a counter there
-        if (self.board[0][colNum] != 0):
+
+        #if number is out of range of len(row) give an error and do not allow user to place counter there
+        while (colNum >= len(self.board[0])):
+            print("Out of range")
+            cn = input(f"Player {playerNum} please enter a column")
+            colNum = int(cn)
+        
+
+        #if all columns are full, then its game over
+        isFull = True
+        for x in self.board[0]:
+            if (self.board[0][x] == 0):
+                isFull = False
+                break
+        
+        if isFull == True:
+            print("All cells full, the game is a draw!")
+            self.gameOver = True
+
+
+        #checks if individual column is full. If full, do not allow user to place a counter there
+        while (self.board[0][colNum] != 0):
             print("Sorry, this column is full, please choose a different column")
             cn = input(f"Player {playerNum} please enter a column")
-            self.addCounter(self, playerNum, int(cn))
+            colNum = int(cn)
+        
 
         else:          
             #finds the first cell that is not a 0. We go from the bottom up
@@ -37,12 +57,11 @@ class board():
                     self.board[j][colNum] = playerNum
                     break
         
-            print(self.board)
             self.checkWin()
+
 
     #checks for a win condition
     def checkWin(self):
-        
         
         for i in range(len(self.board)):
             for j in range(len(self.board[0])):
@@ -52,25 +71,42 @@ class board():
                     #streak = 1 (passed in the last argument when calling checkStreak)
                     value = self.board[i][j]
                     top_left_diag = self.checkStreak(value, i-1, j-1, 1, iop=-1, jop=-1)
+                    if (top_left_diag == True):
+                        return True
                     top_right_diag = self.checkStreak(value, i+1, j-1, 1, iop=1, jop=-1)
+                    if (top_right_diag == True):
+                        return True
                     bot_left_diag = self.checkStreak(value, i-1, j+1, 1, iop=-1, jop=1)
+                    if (bot_left_diag == True):
+                        return True
                     bot_right_diag = self.checkStreak(value, i+1, j+1, 1, iop=1, jop=1)
+                    if (bot_right_diag == True):
+                        return True
                     top = self.checkStreak(value, i, j-1, 1, iop=0, jop=-1)
+                    if (top == True):
+                        return True  
                     bot = self.checkStreak(value, i, j+1, 1, iop=0, jop=1)
+                    if (bot == True):
+                        return True
                     left = self.checkStreak(value, i-1, j, 1, iop=-1, jop=0)
+                    if (left == True):
+                        return True
                     right = self.checkStreak(value, i+1, j, 1, iop=1, jop=0)
+                    if (right == True):
+                        return True
 
                     
 
     #We check if the cell in question is equal to the value. If it is, we call checkStreak again in the same direction.
     def checkStreak(self, number, row, col, streak, iop, jop) -> bool: 
-        #prints out all values in the parameter
-        print(f"number to check: {number}, i: {row}, j: {col}, streak: {streak}. i will be incremented by {iop} and j will be incremented by {jop}")
 
         #check for streak length. If streak = 4, we return True and the game is won and we quit the game
         if streak == 4:
+            self.printboard()
             print(f"Player {number} wins! GG")
+            self.won()
             return True
+
             
         #check for boundary conditions. If out of bounds, we cannot have a streak in the said direction, and we just return false
         elif (row < 0 or col < 0 or row >= len(self.board) or col >= len(self.board[0])):
@@ -86,17 +122,51 @@ class board():
         #If in bounds, but the value of the cell to check is 0, the streak is broken
         else:
             return False
+    
+    #ends the game if the game is determined to have been won
+    def won(self):
+        self.board = [[0 for x in range(len(self.board[0]))]for y in range(len(self.board))]
+        print("THANK YOU FOR PLAYING, I HOPE YOU HAD A NICE DAY :D")
+        self.gameOver = True
+    
+    #prints the board
+    def printboard(self):
+        for row in self.board:
+            print(row)
 
-b = board(19, 5)
-print(b.board)
-b.addCounter(1, 5)
-b.addCounter(1, 3)
-b.addCounter(2, 4)
-b.addCounter(1, 4)
-b.addCounter(2, 2)
-b.addCounter(1, 5)
-b.addCounter(2, 3)
-b.addCounter(1, 5)
-b.addCounter(2, 5)
-b.addCounter(1, 18)
-b.addCounter(2, 4)
+l = input("How many columns do you want your board to be?")
+length = int(l)
+while (length < 5 or length > 30):
+    l = input("Invalid board size, please re-enter a number between 5 and 30 columns")
+    length = int(l)
+
+r = input("How many rows do you want your board to be?")
+width = int(r)
+while (width < 5 or width > 30):
+    r = input("Invalid board size, please re-enter a number between 5 and 30 rows")
+    length = int(r)
+
+p = input("How many players are playing?")
+players = int(p)
+while (players < 2 or players > 9):
+    p = input("Invalid number of players, please pick between 2 to 9 players")
+    players = int(p)
+
+b = board(length, width)
+b.printboard
+
+#keeps looping through the turns as long as the game is still going
+while b.gameOver == False:
+
+    for x in range(1, players + 1):
+        #immediately ends the game if it is declared to be game over
+        if b.gameOver == True:
+            break
+        input1 = input(f"Player {x} please enter the column number you wish to place your counter in. Leftmost column is 0, and rightmost column is {len(b.board[0]) - 1}")
+        colNum1 = int(input1)
+        b.addCounter(x, colNum1)
+        #prints the board at the intermediate stage
+        b.printboard()
+
+
+
